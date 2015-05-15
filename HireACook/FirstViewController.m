d0cff4b08ffb4ca8750d8a1bf9923d064747c0af
Suman Chatterjee <chatterjeesuman@gmail.com>
2015-05-15 17:18:52 +0100

    implement Facebook connect and collecting the graph data

--- HireACook/FirstViewController.m
+++ HireACook/FirstViewController.m
@@ -10,9 +10,10 @@
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
 
-@interface FirstViewController ()
+@interface FirstViewController ()< FBSDKLoginButtonDelegate>
 
 @property (weak, nonatomic) IBOutlet FBSDKLoginButton *loginButton;
+@property (weak, nonatomic) IBOutlet UIImageView *activUser;
 
 @end
 
@@ -21,9 +22,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
    self.loginButton.readPermissions = @[@"public_profile", @"email", @"user_friends"];
+    self.loginButton.delegate = self;
     
     if ([FBSDKAccessToken currentAccessToken]) {
         // User is logged in, do work such as go to next view controller.
+        [self returnUserData];
     }
 
 }
@@ -33,4 +36,67 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - FB Login delegate 
+
+- (void) loginButton:(FBSDKLoginButton *)loginButton didCompleteWithResult:(FBSDKLoginManagerLoginResult *)result error:(NSError *)error
+{
+    //Login result
+    if(error != nil)
+    {
+        //Error
+        NSLog(@"Login error");
+    }
+    else if(result.isCancelled ){
+        //Handle Cancellation
+    }
+    else
+    {
+       // (result.grantedPermissions.co)
+        [self returnUserData];
+    }
+}
+
+- (void)loginButtonDidLogOut:(FBSDKLoginButton *)loginButton
+{
+    //User loged out
+    NSLog(@"User Log OUT");
+}
+
+- (void)returnUserData
+{
+    FBSDKGraphRequest * graphRequest = [[FBSDKGraphRequest alloc] initWithGraphPath:@"me" parameters:nil];
+    [graphRequest startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
+        // handle response
+        if (!error) {
+            // result is a dictionary with the user's Facebook data
+            NSDictionary *userData = (NSDictionary *)result;
+            
+            NSString *facebookID = userData[@"id"];
+            NSString *name = userData[@"name"];
+            NSString *location = userData[@"location"][@"name"];
+            NSString *gender = userData[@"gender"];
+            NSString *birthday = userData[@"birthday"];
+            NSString *relationship = userData[@"relationship_status"];
+            
+            NSURL *pictureURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://graph.facebook.com/%@/picture?type=large&return_ssl_resources=1", facebookID]];
+            
+            NSURLRequest *urlRequest = [NSURLRequest requestWithURL:pictureURL];
+            
+            // Run network request asynchronously
+            [NSURLConnection sendAsynchronousRequest:urlRequest
+                                               queue:[NSOperationQueue mainQueue]
+                                   completionHandler:
+             ^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+                 if (connectionError == nil && data != nil) {
+                     // Set the image in the imageView
+                     // ...
+                     self.activUser.image = [UIImage imageWithData:data];
+                 }
+             }];
+            
+            // Now add the data to the UI elements
+            // ...
+        }
+    }];
+}
 @end
