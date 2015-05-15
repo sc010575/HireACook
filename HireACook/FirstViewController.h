21967952f5f183520e8fbde65a68d24328e27d3c
Suman Chatterjee <chatterjeesuman@gmail.com>
2015-05-15 14:43:38 +0100

    Initial Commit

---
+++ HireACook/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  HireACook
+//
+//  Created by Suman Chatterjee on 15/05/2015.
+//  Copyright (c) 2015 Suman Chatterjee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ HireACook/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  HireACook
+//
+//  Created by Suman Chatterjee on 15/05/2015.
+//  Copyright (c) 2015 Suman Chatterjee. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
